b96e2a23feb27f2bbd3f4bef599c3e0e3682ad4e
LucasJappert <lucasnicolasjappert@gmail.com>
2020-11-16 10:56:50 -0300

    PostWorkbox

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+  // If you provide a version, it will be stored inside cache.
+  // Later when you deploy a new version, old cache will be
+  // automatically purged.
+  version: "Version1",
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
@@ -17,11 +22,11 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg?version=1' },
-      { rel: 'manifest', href: '/manifest.json?version=1' }
+      //{ rel: 'manifest', href: '/manifest.json?version=1' }
     ],
     script: [
       {
-        src: '/main.js',
+        //src: '/main.js',
         //async: true,
         //crossorigin: "anonymous"
       }
@@ -44,16 +49,89 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    '@nuxtjs/pwa',
   ],
+  
+  pwa: {
+    manifest: {
+      start_url: "/",
+      lang: 'es',
+      theme_color: "#FFE400"
+    },
+    icon: {
+      fileName: 'iconoPWA.png',
+    },
+    workbox: {
+      /* workbox options */
+      // workboxURL: "https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js",
+      enabled: true,
+      config: {
+        debug: false,
+      },
+      preCaching: [
+        // { url: "/", revision: "1231232"},
+        { url: "/_nuxt/assets/fonts/proximanova-light.woff2", revision: null},
+        { url: "_nuxt/assets/fonts/proximanova-regular.woff2", revision: null},
+        { url: "/_nuxt/assets/ic_shipping@2x.png", revision: null},
+        { url: "/_nuxt/assets/Logo_ML@2x.png.png", revision: null},
+        { url: "/favicon.svg", revision: null},
+      ]
+    }
+  },
 
   serverMiddleware: ['~/middleware/logger'],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    '@nuxtjs/style-resources', "nuxt-compress"
+    '@nuxtjs/style-resources', 
+    "nuxt-compress",
+    'nuxt-ssr-cache',
   ],
 
-  fetchOnServer: true,
+  ssr: false,
+
+  cache: {
+      // if you're serving multiple host names (with differing
+      // results) from the same server, set this option to true.
+      // (cache keys will be prefixed by your host name)
+      // if your server is behind a reverse-proxy, please use
+      // express or whatever else that uses 'X-Forwarded-Host'
+      // header field to provide req.hostname (actual host name)
+      useHostPrefix: false,
+      pages: [
+        // these are prefixes of pages that need to be cached
+        // if you want to cache all pages, just include '/'
+        //'/page1',
+        //'/page2',
+        '/',
+  
+        // you can also pass a regular expression to test a path
+        ///^\/page3\/\d+$/,
+  
+        // to cache only root route, use a regular expression
+        ///^\/$/
+      ],
+      
+      key(route, context) {
+        // custom function to return cache key, when used previous
+        // properties (useHostPrefix, pages) are ignored. return 
+        // falsy value to bypass the cache
+      },
+  
+      store: {
+        type: 'memory',
+  
+        // maximum number of pages to store in memory
+        // if limit is reached, least recently used page
+        // is removed.
+        max: 100,
+  
+        // number of seconds to store this page in cache
+        ttl: 60,
+      },
+    },
+
+  // fetchOnServer: true,
 
   "nuxt-compress": {
     gzip: {
@@ -69,6 +147,9 @@ export default {
       '~assets/css/variables.scss'
     ]
   },
+
+  
+
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
